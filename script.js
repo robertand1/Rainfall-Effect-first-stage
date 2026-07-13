dc601768bf6d50b2265cc6d23ca16af662888464
robertand1 <126794012+robertand1@users.noreply.github.com>
2026-07-13 19:25:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function createRaindrop() {
+  const raindrop = document.createElement("div");
+  raindrop.classList.add("raindrop");
+  raindrop.style.left = Math.random() * window.innerWidth + "px";
+  const duration = Math.random() * 1 + 0.5;
+  raindrop.style.animationDuration = duration + "s";
+  document.body.appendChild(raindrop);
+  setTimeout(() => {
+    raindrop.remove();
+  }, duration * 1000);
+}
+
+setInterval(createRaindrop, 500);
